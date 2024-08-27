f80080577191db5a5ecaa3ca9a1974542c417b1d
Ashish Bhardwaj <erashish41@gmail.com>
2024-08-27 19:50:45 +0530

    react-router with new and traditiona method

--- 01_react_router/src/App.jsx
+++ 01_react_router/src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from "react-router-dom"
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
@@ -7,25 +7,40 @@ import { Movie } from "./pages/Movie";
 
 const App = () => {
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />
-    },
-    {
-      path: "/about",
-      element: <About />
-    },
-    {
-      path: "/movie",
-      element: <Movie />
-    },
-    {
-      path: "/contact",
-      element: <Contact />
-    },
+  // // New way of React-Router
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home />
+  //   },
+  //   {
+  //     path: "/about",
+  //     element: <About />
+  //   },
+  //   {
+  //     path: "/movie",
+  //     element: <Movie />
+  //   },
+  //   {
+  //     path: "/contact",
+  //     element: <Contact />
+  //   },
+  // ]);
 
-  ]);
+
+  // Traditional (old) way of React-Router
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element = {<Home />} />
+        <Route path="/about" element = {<About />} />
+        <Route path="/movie" element = {<Movie />} />
+        <Route path="/contact" element = {<Contact />} />
+      </Route>
+    )
+  )
+
+
 
   return <RouterProvider router={router} />
   // RouterProvider is a function and router is return to it
